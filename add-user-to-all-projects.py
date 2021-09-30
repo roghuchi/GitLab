b5a39e56fd498b52dc44d6919121eebba06c2af5
Roghuchi <38735518+roghuchi@users.noreply.github.com>
2021-09-30 18:16:31 +0330

    Create add-user-to-all-projects.py

---
+++ add-user-to-all-projects.py
@@ -0,0 +1,24 @@
+import gitlab
+import urllib3
+import re
+
+#vars
+userid= {{Userid}}
+gitLabToken = "{{Token}}"
+gitLabUrl = "{{GitLabUrl}}"
+
+
+#gitlab connection
+gl = gitlab.Gitlab( gitLabUrl, private_token=gitLabToken)
+
+
+#list of groups id is lgi
+groups = gl.groups.list(all=True)
+lgi = re.findall("\d+", str(groups))
+
+for i in lgi:
+    group = gl.groups.get(i)
+    if group.members.get(userid).id == userid:
+      continue
+    else:
+        member = group.members.create({'user_id': userid,'access_level': gitlab.REPORTER_ACCESS})
